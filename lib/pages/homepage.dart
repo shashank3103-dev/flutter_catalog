aff57fe7bf690fe97dac2eeacfe5a576607da10e
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2023-04-03 19:07:14 +0530

    day13completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,29 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final int days = 30;
+
   final String name = "shashank";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decoderData = jsonDecode(catalogJson);
+    var productData = decoderData["products"];
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Padding(
+      body: Padding( 
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          },
-        ),
+        child: (CatalogModel.items !=null && CatalogModel.items.isNotEmpty)
+        ?ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) => 
+             ItemWidget(
+              item: CatalogModel.items[index],
+            ),
+        )
+        :Center(
+          child: CircularProgressIndicator(),
+        )
       ),
       drawer: MyDrawer(),
     );
--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-
 import 'package:flutter_catalog/models/catalog.dart';
 
 class ItemWidget extends StatelessWidget {
