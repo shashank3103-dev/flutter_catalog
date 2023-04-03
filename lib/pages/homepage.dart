a2f131a65655f0dcfe37021c78bc4d890aa64027
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2023-04-04 00:56:19 +0530

    day14completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -38,26 +38,59 @@ class _HomepageState extends State<Homepage> {
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Padding( 
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items !=null && CatalogModel.items.isNotEmpty)
-        ? GridView.builder(
-          gridDelegate: gridDelegate,
-           itemBuilder: itemBuilder, 
-           itemCount: CatalogModel.items.length,
-           )
-        // listView and gridView both are used in this app you can choose in anyone one
-        // ?ListView.builder(
-        //   itemCount: CatalogModel.items.length,
-        //   itemBuilder: (context, index) => 
-        //      ItemWidget(
-        //       item: CatalogModel.items[index],
-        //     ),
-        // )
-        :Center(
-          child: CircularProgressIndicator(),
-        )
-      ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+              ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 18,
+                    crossAxisSpacing:18, 
+                  ),
+                  itemBuilder: (context, index) {
+                    final Item = CatalogModel.items[index];
+                    return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular( 10)
+                        ),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            Item.name, 
+                            style: TextStyle(color: Colors.white
+                            ),),
+                          padding: const EdgeInsets.all(12),
+                          decoration:  BoxDecoration(
+                            color: Colors.deepPurple,
+                          ) ,
+                        ),
+                        child: Image.network(Item.image),
+                        footer:  Container(
+                          child: Text(
+                            Item.price.toString(),
+                            style: TextStyle(color: Colors.white
+                            ),),
+                          padding: const EdgeInsets.all(12),
+                          decoration:  BoxDecoration(
+                            color: Colors.black,
+                          ) ,
+                        ),
+                        ));
+                  },
+                  itemCount: CatalogModel.items.length,
+                )
+              // listView and gridView both are used in this app you can choose in anyone one
+              // ?ListView.builder(
+              //   itemCount: CatalogModel.items.length,
+              //   itemBuilder: (context, index) =>
+              //      ItemWidget(
+              //       item: CatalogModel.items[index],
+              //     ),
+              // )
+              : Center(
+                  child: CircularProgressIndicator(),
+                )),
       drawer: MyDrawer(),
     );
   }
